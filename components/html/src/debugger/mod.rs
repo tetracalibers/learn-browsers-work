9959bcee61407967666aa617c06aebcdf8adbf51
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-01-14 11:08:12 +0900

    html/debugger: body要素内の構造のみを取得する関数を追加

--- components/html/src/debugger/mod.rs
+++ components/html/src/debugger/mod.rs
@@ -29,6 +29,33 @@ pub fn print_dom_tree(document: &NodePtr) {
   document.print_tree(0);
 }
 
+// 兄弟要素も含めて深さ優先で走査
+fn traverse(
+  node: &TreeNode<Node>,
+  depth: usize,
+  node_list_with_depth: &mut Vec<(usize, TreeNode<Node>)>,
+) {
+  // 空文字しかないテキストノード以外をpush
+  let mut append_node = true;
+
+  if let Some(text_node) = node.as_maybe_text() {
+    if text_node.characters.get_data().trim().is_empty() {
+      append_node = false;
+    }
+  }
+
+  if append_node {
+    node_list_with_depth.push((depth, node.clone()));
+  }
+
+  if let Some(first_child) = node.first_child() {
+    traverse(&first_child, depth + 1, node_list_with_depth);
+  }
+  if let Some(next_sibling) = node.next_sibling() {
+    traverse(&next_sibling, depth, node_list_with_depth);
+  }
+}
+
 // build Vec<(usize, TreeNode<Node>)>
 pub fn get_dom_list_with_depth(
   document: &NodePtr,
@@ -39,34 +66,24 @@ pub fn get_dom_list_with_depth(
   let mut node_list_with_depth = Vec::new();
   let depth = 0;
 
-  // 兄弟要素も含めて深さ優先で走査
-  fn traverse(
-    node: &TreeNode<Node>,
-    depth: usize,
-    node_list_with_depth: &mut Vec<(usize, TreeNode<Node>)>,
-  ) {
-    // 空文字しかないテキストノード以外をpush
-    let mut append_node = true;
-
-    if let Some(text_node) = node.as_maybe_text() {
-      if text_node.characters.get_data().trim().is_empty() {
-        append_node = false;
-      }
-    }
-
-    if append_node {
-      node_list_with_depth.push((depth, node.clone()));
-    }
-
-    if let Some(first_child) = node.first_child() {
-      traverse(&first_child, depth + 1, node_list_with_depth);
-    }
-    if let Some(next_sibling) = node.next_sibling() {
-      traverse(&next_sibling, depth, node_list_with_depth);
-    }
-  }
-
   traverse(root, depth, &mut node_list_with_depth);
+
+  node_list_with_depth
+}
+
+pub fn get_dom_list_with_depth_in_body(
+  document: &NodePtr,
+) -> Vec<(usize, TreeNode<Node>)> {
+  let root = document.0.borrow();
+
+  let html = root.first_child().unwrap();
+  let head = html.first_child().unwrap();
+  let body = head.next_sibling().unwrap();
+
+  let mut node_list_with_depth = Vec::new();
+  let depth = 0;
+
+  traverse(&body, depth, &mut node_list_with_depth);
 
   node_list_with_depth
 }
