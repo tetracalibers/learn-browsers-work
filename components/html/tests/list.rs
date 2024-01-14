ffa975e67eeeb95f37e91797a1b2f9b3a1036a56
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-01-15 07:01:46 +0900

    html/tests: description listのparseテスト

--- components/html/tests/list.rs
+++ components/html/tests/list.rs
@@ -97,3 +97,60 @@ fn ordered_list() {
   assert_eq!(dom[7].0, 3);
   assert_eq!(dom[7].1.as_text().characters.get_data(), "item3");
 }
+
+#[test]
+fn description_list() {
+  let html = r#"
+  <dl>
+    <dt>name1</dt>
+    <dd>desc1</dd>
+    <dt>name2</dt>
+    <dd>desc2</dd>
+  </dl>
+  "#;
+
+  // |-Document
+  //   |-Element { data: Unknown(HTMLElement { tag_name: "html" }) }
+  //       |-Element { data: Unknown(HTMLElement { tag_name: "head" }) }
+  //       |-Element { data: Unknown(HTMLElement { tag_name: "body" }) }
+  //           |-Element { data: Unknown(HTMLElement { tag_name: "dl" }) }
+  //               |-Element { data: Unknown(HTMLElement { tag_name: "dt" }) }
+  //                   |-Text("name1")
+  //               |-Element { data: Unknown(HTMLElement { tag_name: "dd" }) }
+  //                   |-Text("desc1")
+  //               |-Element { data: Unknown(HTMLElement { tag_name: "dt" }) }
+  //                   |-Text("name2")
+  //               |-Element { data: Unknown(HTMLElement { tag_name: "dd" }) }
+  //                   |-Text("desc2")
+
+  let document = get_document_from_html(html);
+  // Vec<(depth, TreeNode<Node>)>
+  let dom = get_dom_list_with_depth_in_body(&document);
+
+  assert_eq!(dom[1].0, 1);
+  assert_eq!(dom[1].1.as_element().tag_name(), "dl");
+
+  assert_eq!(dom[2].0, 2);
+  assert_eq!(dom[2].1.as_element().tag_name(), "dt");
+
+  assert_eq!(dom[3].0, 3);
+  assert_eq!(dom[3].1.as_text().characters.get_data(), "name1");
+
+  assert_eq!(dom[4].0, 2);
+  assert_eq!(dom[4].1.as_element().tag_name(), "dd");
+
+  assert_eq!(dom[5].0, 3);
+  assert_eq!(dom[5].1.as_text().characters.get_data(), "desc1");
+
+  assert_eq!(dom[6].0, 2);
+  assert_eq!(dom[6].1.as_element().tag_name(), "dt");
+
+  assert_eq!(dom[7].0, 3);
+  assert_eq!(dom[7].1.as_text().characters.get_data(), "name2");
+
+  assert_eq!(dom[8].0, 2);
+  assert_eq!(dom[8].1.as_element().tag_name(), "dd");
+
+  assert_eq!(dom[9].0, 3);
+  assert_eq!(dom[9].1.as_text().characters.get_data(), "desc2");
+}
