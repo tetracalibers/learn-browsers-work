8d00be249e58794f8460502a8dc866bce9a8e25b
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-01-23 10:58:37 +0900

    main: サンプルHTML変更

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,28 @@ const TARGET_HTML: &str = r#"<!DOCTYPE html>
 <body>
 <table>
   <caption>
-    Example Caption
+    Superheros and sidekicks
   </caption>
+  <colgroup>
+    <col />
+    <col span="2" class="batman" />
+    <col span="2" class="flash" />
+  </colgroup>
   <tr>
-    <th>Login</th>
-    <th>Email</th>
+    <td> </td>
+    <th scope="col">Batman</th>
+    <th scope="col">Robin</th>
+    <th scope="col">The Flash</th>
+    <th scope="col">Kid Flash</th>
   </tr>
   <tr>
-    <td>user1</td>
-    <td>user1@sample.com</td>
-  </tr>
-  <tr>
-    <td>user2</td>
-    <td>user2@sample.com</td>
+    <th scope="row">Skill</th>
+    <td>Smarts</td>
+    <td>Dex, acrobat</td>
+    <td>Super speed</td>
+    <td>Super speed</td>
   </tr>
 </table>
-
 
 </body>
 </html>"#;
