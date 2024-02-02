d3979ac10ff0a386ba558ad937a7b97b25bfc808
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-02-03 06:50:42 +0900

    css/parser: エスケープされた引用符を含む引用符囲み文字列パーサーを実装

--- components/css/src/parser/utility.rs
+++ components/css/src/parser/utility.rs
@@ -1,14 +1,18 @@
 use nom::branch::alt;
+use nom::bytes::complete::escaped_transform;
 use nom::bytes::complete::tag;
 use nom::bytes::complete::take_until;
 use nom::character::complete::alpha0;
 use nom::character::complete::alpha1;
 use nom::character::complete::char;
 use nom::character::complete::newline;
+use nom::character::complete::none_of;
 use nom::character::complete::space0;
 use nom::combinator::map;
 use nom::combinator::opt;
+use nom::combinator::value;
 use nom::multi::many0;
+use nom::sequence::delimited;
 use nom::sequence::tuple;
 use nom::IResult;
 
@@ -45,4 +49,75 @@ pub fn single_quoted(input: &str) -> IResult<&str, &str> {
 
 pub fn quoted(input: &str) -> IResult<&str, &str> {
   alt((double_quoted, single_quoted))(input)
+}
+
+// 例："this is a \"string\"."
+pub fn double_quoted_within_esceped_quote(
+  input: &str,
+) -> IResult<&str, String> {
+  delimited(
+    tag("\""),
+    escaped_transform(none_of("\\\""), '\\', value("\"", tag("\""))),
+    tag("\""),
+  )(input)
+}
+
+pub fn single_quoted_within_esceped_quote(
+  input: &str,
+) -> IResult<&str, String> {
+  delimited(
+    tag("'"),
+    escaped_transform(none_of("\\'"), '\\', value("'", tag("'"))),
+    tag("'"),
+  )(input)
+}
+
+pub fn quoted_within_esceped_quote(input: &str) -> IResult<&str, String> {
+  alt((
+    double_quoted_within_esceped_quote,
+    single_quoted_within_esceped_quote,
+  ))(input)
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_space_with_newline() {
+    assert_eq!(space_with_newline(" \n "), Ok(("", "")));
+    assert_eq!(space_with_newline(" "), Ok(("", "")));
+    assert_eq!(space_with_newline("\n"), Ok(("", "")));
+  }
+
+  #[test]
+  fn test_alpha1_with_hyphen() {
+    assert_eq!(alpha1_with_hyphen("a"), Ok(("", "a".to_string())));
+    assert_eq!(alpha1_with_hyphen("a-b"), Ok(("", "a-b".to_string())));
+    assert_eq!(alpha1_with_hyphen("a-b-c"), Ok(("", "a-b-c".to_string())));
+  }
+
+  #[test]
+  fn test_double_quoted_within_esceped_quote() {
+    assert_eq!(
+      double_quoted_within_esceped_quote(r#""this is a 'string'.""#),
+      Ok(("", "this is a 'string'.".to_string()))
+    );
+    assert_eq!(
+      double_quoted_within_esceped_quote(r#""this is a \"string\".""#),
+      Ok(("", "this is a \"string\".".to_string()))
+    );
+  }
+
+  #[test]
+  fn test_single_quoted_within_esceped_quote() {
+    assert_eq!(
+      single_quoted_within_esceped_quote(r#"'this is a "string".'"#),
+      Ok(("", "this is a \"string\".".to_string()))
+    );
+    assert_eq!(
+      single_quoted_within_esceped_quote(r#"'this is a \'string\'.'"#),
+      Ok(("", "this is a 'string'.".to_string()))
+    );
+  }
 }
