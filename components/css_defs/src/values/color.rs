c9745b54a0fb7447b9fb9fc01ae36ff1276585f1
tetracalibers <tomix05may@gmail.com>
2024-06-12 10:52:05 +0900

    Colorにparseメソッドを実装

--- components/css_defs/src/values/color.rs
+++ components/css_defs/src/values/color.rs
@@ -6,3 +6,24 @@ pub enum Color {
   Rgba(Number, Number, Number, Number),
   Transparent,
 }
+
+impl Color {
+  pub fn parse(value: String) -> Option<Self> {
+    match value.to_lowercase().as_str() {
+      "currentcolor" => Some(Color::CurrentColor),
+      "transparent" => Some(Color::Transparent),
+      _ => {
+        let result = csscolorparser::parse(value.as_str());
+        match result {
+          Ok(color) => Some(Color::Rgba(
+            Number(color.r as f32),
+            Number(color.g as f32),
+            Number(color.b as f32),
+            Number(color.a as f32),
+          )),
+          Err(_) => None,
+        }
+      }
+    }
+  }
+}
