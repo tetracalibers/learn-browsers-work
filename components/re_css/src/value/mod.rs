9c50484cd183c7786588497d7e2c75faf55bf859
tetracalibers <tomix05may@gmail.com>
2024-07-16 21:35:53 +0900

    Value::Percentageを実装

--- components/re_css/src/value/mod.rs
+++ components/re_css/src/value/mod.rs
@@ -1,11 +1,14 @@
 use length::Length;
+use percentage::Percentage;
 
 use crate::token::CSSToken;
 
 pub mod length;
+pub mod percentage;
 
 pub enum Value {
   Length(Length),
+  Percentage(Percentage),
   // css wide keywords
   Inherit,
   Initial,
---
+++ components/re_css/src/value/percentage.rs
@@ -0,0 +1,14 @@
+use crate::token::CSSToken;
+
+use super::{Value, ValueParser};
+
+pub struct Percentage(f64);
+
+impl ValueParser for Percentage {
+  fn parse(token: CSSToken) -> Option<Value> {
+    match token {
+      CSSToken::Percentage(value) => Some(Value::Percentage(Percentage(value))),
+      _ => None,
+    }
+  }
+}
